acab7ba8da50d4bc65688059985be8738f232f56
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-06-26 08:49:00 -0300

    Create MainUm for Primos.

---
+++ MainUm.java
@@ -0,0 +1,5 @@
+class MainUm{
+    public static void main (String args[]){
+    Primos p= new Primos();
+    }
+}
--- Primos.java
+++ Primos.java
@@ -3,9 +3,9 @@ class   Primos{
 
     public Primos(){
         Scanner  s = new Scanner(System.in);
-        System.out.println("Digite a quantidade de primos desajados");
+        System.out.print("Digite a quantidade de primos desajados: ");
         int n = s.nextInt();
-        System.out.println("Digite o intervalo desejado:");
+        System.out.print("Digite o intervalo desejado (dois valores): ");
         int a = s.nextInt();
         int b = s.nextInt();
     }

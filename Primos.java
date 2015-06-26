857315b0514b6c9ff463e9ac9c82f8908c2551c8
Harold Becker <Hcsb20@gmail.com>
2015-06-26 08:33:19 -0300

    classe primos

---
+++ Primos.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class   Primos{
+
+    public Primos(){
+        Scanner  s = new Scanner(System.in);
+        System.out.println("Digite a quantidade de primos desajados");
+        int n = s.nextInt();
+        System.out.println("Digite o intervalo desejado:");
+        int a = s.nextInt();
+        int b = s.nextInt();
+    }
+
+}

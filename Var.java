9485dd50fae7bbf7265f5f91db61f1253a6d3dce
vladimirbelinski <vladimir_belinski@hotmail.com>
2015-06-26 09:07:41 -0300

    Added class for extra.

---
+++ Var.java
@@ -0,0 +1,32 @@
+class var{
+    public int getVar1(){
+        return 1;
+    }
+    public int getVar2(){
+        return 2;
+    }
+    public int getVar3(){
+        return 3;
+    }
+    public int getVar4(){
+        return 4;
+    }
+    public int getVar5(){
+        return 5;
+    }
+    public int getVar6(){
+        return 6;
+    }
+    public int getVar7(){
+        return 7;
+    }
+    public int getVar8(){
+        return 8;
+    }
+    public int getVar9(){
+        return 9;
+    }
+    public int getVar10(){
+        return 10;
+    }
+}
